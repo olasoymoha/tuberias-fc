ece4c9f87ff9f086667239b9474b01497ad8376b
Juan Rodríguez <juamcamirodri@gmail.com>
2024-05-24 18:23:54 -0500

    40%

--- src/app/UI/components/inicio/inicio.component.ts
+++ src/app/UI/components/inicio/inicio.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NoticiasComponent } from './noticias/noticias.component';
+import { TablaComponent } from './tabla/tabla.component';
+import { PartidosComponent } from './partidos/partidos.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [NoticiasComponent],
+  imports: [NoticiasComponent, TablaComponent, PartidosComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
---
+++ src/app/interfaces/equipo.ts
@@ -0,0 +1,13 @@
+export interface Equipo {
+    id: number;
+    nombre: string;
+    posicion: number;
+    victorias: number;
+    empates: number;
+    derrotas: number;
+    partidos: number;
+    golesFavor: number;
+    golesContra: number;
+    puntos: number;
+    imagen: string
+}
