11cc2956fc832bae7a260125409bf8acf00a4731
Moha <mohamedcisse2003@gmail.com>
2024-05-24 12:56:06 +0200

    primer commit

---
+++ php/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header con Flexbox</title>
+   <link rel="stylesheet" href="../css/header.css">
+</head>
+<body>
+    <header>
+        <div class="container1">
+            <img src="../img/logo-tuberias.png">
+        </div>
+        <div class="container2">
+            <nav>
+                <a href="inicio.html">Inicio</a>
+                <a href="#about">Acerca de</a>
+                <a href="#services">Servicios</a>
+                <a href="#contact">Contacto</a>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
